7fbaec4676b0c6a553d490720f276b2d172c0c7d
JorgeHCT <bompskiller@gmail.com>
2026-04-27 21:36:40 +0200

    fix supabase server and other

--- lib/supabase-server.ts
+++ lib/supabase-server.ts
@@ -1,5 +1,7 @@
-import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
-import { cookies } from 'next/headers'
+import { createClient } from '@supabase/supabase-js'
 
 export const createServerClient = () =>
-  createServerComponentClient({ cookies })
+  createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  )
